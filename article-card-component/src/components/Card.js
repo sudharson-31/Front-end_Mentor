1a2c5718e0d8c43990679835c817183c6eb49621
sudharson-31 <96968997+sudharson-31@users.noreply.github.com>
2022-06-04 16:32:19 +0530

    main-card

---
+++ article-card-component/src/components/Card.js
@@ -0,0 +1,32 @@
+export default function Card(){
+    return (
+        <div className="main-box">
+            <div className="banner-image-container">
+                <img id="banner-image" src={process.env.PUBLIC_URL + "/images/drawers.jpg"} alt="drawers"></img>
+            </div>
+            <div className="content-box">
+                <div id="heading">
+                    <h4>Shift the overall look and feel by adding these wonderful touches to furniture in your home</h4>
+                </div>
+                <div id="content">
+                    <p>Ever been in a room and felt like something was missing? 
+                    Perhaps it felt slightly bare and uninviting. 
+                    I’ve got some simple tips to help you make any room feel complete.
+                    </p>
+                </div>
+                <div id="profile">
+                    <span id="profile-image-container">
+                        <img id="profile-image" src={process.env.PUBLIC_URL + "/images/avatar-michelle.jpg"} alt="profile-picture"></img>
+                    </span>
+                    <div id="details">
+                        <span id="name">Michelle Appleton</span>
+                        <span id="date">28 Jun 2020</span>
+                    </div>
+                    <span id="share-image-container">
+                        <img id="share-image" src={process.env.PUBLIC_URL + "/images/icon-share.svg"} alt="share"></img>
+                    </span>
+                </div>
+            </div>
+        </div>
+    );
+}
